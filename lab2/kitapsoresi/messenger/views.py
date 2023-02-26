60b7a5cd75a0f4aeb9dbd4972b98be30c88bb0f0
Maksat <105899668+Baikadamov@users.noreply.github.com>
2023-02-26 22:28:26 +0600

    Add files via upload

---
+++ lab2/kitapsoresi/messenger/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+
+from messenger.models import Books, Genre, Author
+
+menu = [
+    {'title ': "Книги", 'url_name': 'books'},
+    {'title': "О нас", 'url_name ': 'about'},
+]
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Glavnaya Stranica </h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>about</h1>')
+    # return render(request, 'messenger/errors/500.html', status=500)
+
+
+def categories(request, catid):
+    return HttpResponse(f"<h1>Categories<br></h1>  <p>{catid}</p>")
+
+
+def error400(request, exception):
+    return HttpResponseNotFound('<h1>Page not found 400</h1>')
+    # return render(request, 'messenger/errors/400.html', status=400)
+
+
+def error403(request, exception):
+    return HttpResponseNotFound('<h1>Page not found 403 </h1>')
+    # return render(request, 'messenger/errors/403.html', status=403)
+
+
+def error404(request, exception):
+    return HttpResponseNotFound('<h1>Page not found 404</h1>')
+
+
+def error500(request):
+    return HttpResponseNotFound('<h1>Page not found 500</h1>')
+    # return render(request, 'messenger/errors/500.html', status=500)
